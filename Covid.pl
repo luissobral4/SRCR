1973642dc70c81ad25e240f33b0bf21bcde546c3
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2021-04-08 16:35:11 +0100

    Organização do codigo

--- Covid.pl
+++ Covid.pl
@@ -1,4 +1,3 @@
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Base de Conhecimento com informacao utentes, centro_saude, staff,vacinaçao.
 
 
@@ -7,7 +6,6 @@
 :- set_prolog_flag( single_var_warnings,off ).
 :- set_prolog_flag( unknown,fail ).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Definicoes iniciais
 
 :- op( 900,xfy,'::' ).
@@ -18,8 +16,10 @@
 :- dynamic data/3.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado utente : ID Utente, Segurança_Social, Nome, Data_Nasc, Email, Telefone, Morada, Profissão, LDoencas, CentroSaúde -> {V,F}
+% POVOAMENTO
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
+% Extensao do predicado utente : ID Utente, Segurança_Social, Nome, Data_Nasc, Email, Telefone, Morada, Profissão, LDoencas, CentroSaúde -> {V,F}
 utente(1,21455655,'Luís',1970,'luis@gmail.com',936696454,'Adaúfe','Estudante',['Doença coronária','Doença respiratória'],1). % mais 50 e doença 1 - fase 1
 utente(2,31455655,'Filipe',1969,'filipe@gmail.com',936696100,'Barcelos','Estudante',['Diabetes'],1). % mais 50 e doença 2 - fase 2
 utente(3,41455655,'José',1990,'j@gmail.com',936696151,'Esposende','Estudante',[],1). % menos de 50 - fase 3
@@ -30,24 +30,18 @@ utente(7,75355655,'Beatriz',1995,'b@gmail.com',936973645,'Aveiro','Enfermeiro',[
 utente(8,43455692,'Maria',1951,'m@gmail.com',93718248,'Braga','Estudante',[],2). % 65 a 80 - fase 2
 utente(9,42487435,'Carlos',1983,'c@gmail.com',936827634,'Aveiro','Estudante',['Arteroses'],2). % doença invalida - fase 3
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado 'centro_saude' : ID Centro, Nome, Morada, Telefone, 
 % Email -> {V,F}
-
 centro_saude(1,'BragaH','Braga','999999999','bragah@gmail.com').
 centro_saude(2,'GuimaraesH','GuimaraesH','999999998','guimaraesh@gmail.com').
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado 'staff' : ID staff, ID Centro, Nome, Email -> {V,F}
-
 staff(1,1,'Luís','luis@gmail.com').
 staff(2,2,'Paulo','paulo@gmail.com').
 staff(3,1,'Francisco','francisco@gmail.com').
 staff(4,2,'João','joao@gmail.com').
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado 'vacinacao_Covid' : ID staff, ID Utente, Data, Vacina, Toma -> {V,F}
-
 % Utente 1 - 2 vacinas fase 1
 vacinacao_Covid(1, 1, 11, 1, 2021, 'AstraZeneca', 1).
 vacinacao_Covid(1, 1, 11, 4, 2021, 'AstraZeneca', 2).
@@ -68,12 +62,13 @@ vacinacao_Covid(2, 7, 1, 3, 2021, 'AstraZeneca', 1).
 % Utente 9 - não vacinado
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Invariante Estrutural:  nao permite a insercao de conhecimento repetido
+% INVARIANTES
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
+% Invariante Estrutural:  nao permite a insercao de conhecimento repetido
 +utente(ID,Q,Nome,Da,E,T,M,P,D,C)::
 	(solucoes((ID,Q,Nome,Da,E,T,M,P,D,C),utente(ID,Q,Nome,Da,E,T,M,P,D,C),Lista),
 	comprimento(Lista,N), N==1).
-
 
 +centro_saude(Id,Nome,Morada,Tel,Email)::
 	(solucoes((Id,Nome,Morada,Tel,Email),centro_saude(Id,Nome,Morada,Tel,Email),Lista),
@@ -87,7 +82,6 @@ vacinacao_Covid(2, 7, 1, 3, 2021, 'AstraZeneca', 1).
 	(solucoes((ID_Staff,ID_Utente,Dia,Mes,Ano,Vacina,Toma),vacinacao_Covid(ID_Staff,ID_Utente,Dia,Mes,Ano,Vacina,Toma),Lista),
 	comprimento(Lista,N), N==1).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Invariante Referencial: nao admite ids repetidos
 
 +utente(ID,Q,Nom,Data,E,T,M,P,D,C)::
@@ -138,9 +132,11 @@ vacinacao_Covid(2, 7, 1, 3, 2021, 'AstraZeneca', 1).
 	(solucoes(Id,centro_saude(Id,Nome,Morada,Tel,Email),Lista),
 	comprimento(Lista,N), N==1).
 
+
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % remover utentes, CentroSaúde , staff e Vacinas:
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
 removeUtente(ID,Q,No,Da,E,T,M,P,D,C) :- involucao(utente(ID,Q,No,Da,E,T,M,P,D,C)).
 
 removeCentro(ID,N,M,T,E) :- involucao(centro_saude(ID,N,M,T,E)).
@@ -312,7 +308,10 @@ inferencia(Questao, verdadeiro) :- Questao.
 inferencia(Questao, falso) :- -Questao.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% EVOLUÇÃO E INVOLUÇÃO
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
+% Extensao do predicado que permite a evolução do conhecimento
 evolucao( Termo ) :-
     solucoes( Invariante,+Termo::Invariante,Lista ),
     insercao( Termo ),
@@ -329,9 +328,7 @@ teste( [R|LR] ) :-
     teste( LR ).
 
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado que permite a involucao do conhecimento
-
 involucao( Termo ) :-
     solucoes( Invariante,-Termo::Invariante,Lista ),
     remocao( Termo ),
@@ -343,55 +340,49 @@ remocao( Termo ) :-
     assert( Termo ),!,fail.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Predicados auxiliares
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
 % Extensao do meta-predicado nao: Questao -> {V,F}
 nao( Questao ) :- Questao, !, fail.
 nao( Questao ).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado que permite verificar se um elemento percente a uma lista: 
 % 'contains':  Elemento, Conjunto -> {V,F}
 contains(E,[E|T]).
 contains(E,[Y|T]) :- E\=Y, contains(E,T).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado que permite verificar se um numero é natural: 
 % 'natural':  Numero -> {V,F}
 natural(1).
 natural(N) :- M is N-1 , natural(M).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado «adicionar» que insere um elemento numa lista, sem o repetir: Elemento,Lista,Resultado -> {V,F}
 add(E,L,L) :- contains(E,L).
 add(E, L, [E|L]).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado «concatenar», que resulta na concatenação dos elementos da lista L1 com os elementos da lista L2: Lista1,Lista2,Resultado -> {V,F}
 concat([], L, L).
 concat([H|T], L, R) :- add(H, N, R), concat(T, L, N).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado 'solucoes'
 solucoes( X,Y,Z ) :-
     findall( X,Y,Z ).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado 'solucoes'
 comprimento( S,N ) :-
     length( S,N ).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado 'removeRepetidos' que remove os elementos repetidos duma lista
+removeRepetidos([], []).
+removeRepetidos([H|T], R) :- contains(H, T) , removeRepetidos(T, R). 
+removeRepetidos([H|T], [H|R]) :- nao(contains(H, T)) , removeRepetidos(T, R).
+
 % Extensão do predicado 'bissexto': Ano => {V, F}
 bissexto(X) :- X mod 4 == 0.
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensão do predicado 'data': Ano, Mes, Dia => {V, F}
 data(A,M,D) :- M\=2, A>=2010, contains(M,[1,3,5,7,8,10,12]), D>=0, D=<31.
 data(A,M,D) :- M\=2, A>=2010, contains(M,[4,6,9,11]), D>0, D=<30.
 data(A,M,D) :- M==2 , bissexto(A), A>=2010, D>0, D=<29.
 data(A,M,D) :- M==2 , nao(bissexto(A)), A>=2010, D>0, D=<28.
-
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensão do predicado 'removeRepetidos' que remove os elementos repetidos duma lista
-removeRepetidos([], []).
-removeRepetidos([H|T], R) :- contains(H, T) , removeRepetidos(T, R). 
-removeRepetidos([H|T], [H|R]) :- nao(contains(H, T)) , removeRepetidos(T, R).
